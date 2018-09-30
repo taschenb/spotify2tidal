d01964ace8dbb8aa726f88899f9cdce8de46ca58
taschenb <taschenb@posteo.de>
2018-09-30 04:13:47 +0200

    Include spotify and tidal in spotify2tidal

---
+++ spotify2tidal/spotify2tidal.py
@@ -0,0 +1,67 @@
+from spotify2tidal.spotify import Spotify
+from spotify2tidal.tidal import Tidal
+
+
+class Spotify2tidal:
+    def __init__(
+        self,
+        tidal_username,
+        tidal_password,
+        spotify_username,
+        spotify_client_id,
+        spotify_client_secret,
+        spotify_redirect_uri,
+        spotify_discover_weekly_id=None,
+    ):
+        self.spotify = Spotify(
+            spotify_username,
+            spotify_client_id,
+            spotify_client_secret,
+            spotify_redirect_uri,
+            spotify_discover_weekly_id,
+        )
+        self.tidal = Tidal(tidal_username, tidal_password)
+
+    def all_spotify_playlists_to_tidal(self):
+        """Create all your spotify playlists in tidal.
+        """
+        for playlist in self.spotify.own_playlists:
+            self._tidal_add_spotify_playlist(
+                spotify_playlist=playlist, delete_existing=True
+            )
+
+    def discover_weekly_to_tidal(self):
+        """Create a discover weekly in tidal.
+        """
+        self._tidal_add_spotify_playlist(
+            self.spotify.discover_weekly_playlist,
+            playlist_name="Discover Weekly",
+            delete_existing=True,
+        )
+
+    def _tidal_add_spotify_playlist(
+        self, spotify_playlist, playlist_name=None, delete_existing=False
+    ):
+        """Create a tidal playlist and copy available tracks.
+
+        Keyword arguments:
+        spotify_playlist: Playlist to copy to tidal
+        playlist_name: Overwrite the playlist name (optional)
+        delete_existing: Delete any existing playlist with the same name
+        """
+        if playlist_name is None:
+            playlist_name = spotify_playlist["name"]
+
+        spotify_tracks = self.spotify.tracks_from_playlist(spotify_playlist)
+
+        tidal_playlist_id = self.tidal._create_playlist(
+            playlist_name, delete_existing
+        )
+
+        for track in spotify_tracks:
+            artist = track["track"]["artists"][0]["name"]
+            # album = track["track"]["album"]["name"]
+            name = track["track"]["name"]
+            self.tidal.add_track_to_playlist(
+                tidal_playlist_id, artist=artist, name=name
+            )
