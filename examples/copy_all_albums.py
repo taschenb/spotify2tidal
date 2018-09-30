983e838dc9e17fe7890b281e480ae62b26f64aad
taschenb <taschenb@posteo.de>
2018-09-30 16:30:24 +0200

    Examples: Add copy all saved albums
    Copy all saved albums from Spotify to Tidal.

---
+++ examples/copy_all_albums.py
@@ -0,0 +1,28 @@
+import logging
+from spotify2tidal import Spotify2tidal
+
+import config
+
+if __name__ == "__main__":
+    """Copy all saved albums from Spotify to Tidal.
+
+    This does not include special playlists like 'Discover Weekly'.
+
+    In order to work, a Spotify client has to be registered and the
+    corresponding ID, secret and redirection URI have to be set in the
+    configuration file 'config.py'.
+    """
+    # Enable logging to see what is going on
+    logging.getLogger("spotify2tidal").addHandler(logging.StreamHandler())
+    logging.getLogger("spotify2tidal").setLevel(logging.INFO)
+
+    st = Spotify2tidal(
+        config.tidal_username,
+        config.tidal_password,
+        config.spotify_username,
+        config.spotify_client_id,
+        config.spotify_client_secret,
+        config.spotify_client_redirect_uri,
+    )
+
+    st.copy_all_saved_spotify_albums()
